51708ccf42998801de135b750cef187592ca9bac
joinsung <63231874+joinsung@users.noreply.github.com>
2020-10-26 15:50:30 +0900

    10/20
    FantasyLand

--- Assets/02.Scripts/LookItem.cs
+++ Assets/02.Scripts/LookItem.cs
@@ -6,6 +6,23 @@ using UnityEngine.EventSystems;
 public class LookItem : MonoBehaviour, IPointerEnterHandler,
     IPointerExitHandler, IGvrPointerHoverHandler
 {
+    private Animator anim;
+    private readonly int hashIsOpen = Animator.StringToHash("IsOpen");
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void OnBoxOpen(bool isOpen)
+    {
+        anim.SetBool(hashIsOpen, isOpen);
+    }
     public void OnLookItemBox(bool isLookAt)
     {
         Debug.Log(isLookAt);
